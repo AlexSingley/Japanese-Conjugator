29e424226aaa03532b6acfe963bf3595a6f5d7c7
Alex Singley <singleyalex@gmail.com>
2018-08-27 10:06:42 -0400

    Beginning to add a UI

---
+++ Japanese Conjugator/src/conjugator/ConjugationViewerController.java
@@ -0,0 +1,26 @@
+package conjugator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jfoenix.controls.JFXListView;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class ConjugationViewerController {
+
+    @FXML
+    private AnchorPane verbConjugatorPanel;
+    
+    @FXML
+    private JFXListView<String> conjugationList;
+    
+	@FXML
+    void setInput() {
+    	
+    }
+
+}
