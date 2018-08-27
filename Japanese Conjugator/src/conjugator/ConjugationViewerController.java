0989946deb54bc85c7e929c5ab4c131a1e56db2e
Alex Singley <singleyalex@gmail.com>
2018-08-27 14:29:24 -0400

    Added conjugation options to list.

--- Japanese Conjugator/src/conjugator/ConjugationViewerController.java
+++ Japanese Conjugator/src/conjugator/ConjugationViewerController.java
@@ -1,16 +1,20 @@
 package conjugator;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXListView;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.AnchorPane;
 
-public class ConjugationViewerController {
+public class ConjugationViewerController extends AnchorPane implements Initializable {
 
     @FXML
     private AnchorPane verbConjugatorPanel;
@@ -22,5 +26,14 @@ public class ConjugationViewerController {
     void setInput() {
     	
     }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+    	conjugationList.getItems().addAll("1. Non-Past Plain", "2. Non-Past Polite", "3. Past Plain",
+    			"4. Past Polite", "5. Te-Form", "6. Potential Plain", "7. Potential Polite", "8. Passive Plain",
+    			"9. Passive Polite", "10. Causative Plain", "11. Causative Polite", "12. Causative Passive Plain",
+    			"13. Causative Passive Polite", "14. Imperative", "15. Negative Imperative");
+    	conjugationList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+	}
 
 }
